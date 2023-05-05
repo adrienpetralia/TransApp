dcb84ab09714df0d867b819f006697f3a0109118
Adrien Petralia <adrien.petralia@gmail.com>
2023-05-05 10:37:21 +0200

    Update Framework.py

--- src/AD_Framework/Framework.py
+++ src/AD_Framework/Framework.py
@@ -86,7 +86,6 @@ class TSDataset(torch.utils.data.Dataset):
         
         
         
-        
 
 # ====================================== Imbalance Metric Class ====================================== #        
 class getmetrics():
@@ -1461,3 +1460,23 @@ class AD_Framework_Sktime(BasedClassifTrainer_Sktime):
             return best_metrics, y, y_hat, y_hat_prob
         else:
             return best_metrics
+
+        
+        
+# ====================================== EarlyStopper ====================================== #
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
